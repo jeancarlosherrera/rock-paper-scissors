ae01b5ecff907978a53e1cc4bd2ff78cf48e8942
Jean C. Herrera <jeancarlosherreram@gmail.com>
2021-11-10 13:06:09 -0500

    added scoreboard

--- app.js
+++ app.js
@@ -2,45 +2,78 @@ const playButton = document.getElementById("play");
 const weaponSelector = document.getElementById("weapon");
 let playerImage = document.getElementById("player-img");
 let machineImage = document.getElementById("machine-img");
-let banner = document.querySelector(".banner")
+let banner = document.querySelector(".banner");
+let playerScoreDisplay = document.getElementById("player-score");
+let machineScoreDisplay = document.getElementById("machine-score");
 
 const machineWeapon = () => {
     let rand = Math.floor(Math.random() * 3) + 1;
     return rand === 1 ? "Rock" : rand === 2 ? "Paper" : "Scissors";
 }
 
+let playerScore = 0;
+let machineScore = 0;
+let isGameOver = false;
+let winningScore = 3;
+
 const play = () => {
-    let result;
-    const playerWeapon = weapon.value;
-    let machineResult = machineWeapon();
-    machineWeapon();
-    if (playerWeapon === machineResult) {
-        result = "Draw";
-        machineImage.src = playerImage.src;
-    } else if (playerWeapon === "Rock" && machineResult === "Scissors") {
-        result = "Rock destroys Scissors. You Won.";
-        machineImage.src = "scissors.png";
-    } else if (playerWeapon === "Scissors" && machineResult === "Rock") {
-        result = "Rock destroys Scissors. Machine Won.";
-        machineImage.src = "rock.png";
-    } else if (playerWeapon === "Paper" && machineResult === "Rock") {
-        result = "Paper wraps Rock. You Won.";
-        machineImage.src = "rock.png";
-    } else if (playerWeapon === "Rock" && machineResult === "Paper") {
-        result = "Paper wraps Rock. Machine Won.";
-        machineImage.src = "paper.png";
-    } else if (playerWeapon === "Scissors" && machineResult === "Paper") {
-        result = "Scissors cut Paper. You Won.";
-        machineImage.src = "paper.png";
-    } else if (playerWeapon === "Paper" && machineResult === "Scissors") {
-        result = "Scissors cut Paper. Machine Won.";
-        machineImage.src = "scissors.png";
+    if (!isGameOver) {
+        let result;
+        const playerWeapon = weapon.value;
+        let machineResult = machineWeapon();
+        machineWeapon();
+        if (playerWeapon === machineResult) {
+            result = "Draw";
+            machineImage.src = playerImage.src;
+        } else if (playerWeapon === "Rock" && machineResult === "Scissors") {
+            result = "Rock destroys Scissors";
+            machineImage.src = "scissors.png";
+            playerScore += 1;
+        } else if (playerWeapon === "Scissors" && machineResult === "Rock") {
+            result = "Rock destroys Scissors";
+            machineImage.src = "rock.png";
+            machineScore += 1;
+        } else if (playerWeapon === "Paper" && machineResult === "Rock") {
+            result = "Paper wraps Rock";
+            machineImage.src = "rock.png";
+            playerScore += 1;
+        } else if (playerWeapon === "Rock" && machineResult === "Paper") {
+            result = "Paper wraps Rock";
+            machineImage.src = "paper.png";
+            machineScore += 1;
+        } else if (playerWeapon === "Scissors" && machineResult === "Paper") {
+            result = "Scissors cut Paper";
+            machineImage.src = "paper.png";
+            playerScore += 1;
+        } else if (playerWeapon === "Paper" && machineResult === "Scissors") {
+            result = "Scissors cut Paper";
+            machineImage.src = "scissors.png";
+            machineScore += 1;
+        }
+
+        const winner = document.createElement("li");
+        banner.appendChild(winner)
+        winner.textContent = result;
+        machineScoreDisplay.textContent = machineScore;
+        playerScoreDisplay.textContent = playerScore;
+        playButton.innerText = "Next Round"
+
+        if (playerScore === 3) {
+            isGameOver = true;
+            result = "You Won!"
+            weaponSelector.disabled = true;
+            winner.textContent = result;
+            const playAgainButton = document.createElement("button");
+            banner.appendChild(playAgainButton);
+        } else if (machineScore === 3) {
+            isGameOver = true;
+            result = "Machine Won!"
+            weaponSelector.disabled = true;
+            winner.textContent = result;
+            const playAgainButton = document.createElement("button");
+            banner.appendChild(playAgainButton);
+        }
     }
-    const winner = document.createElement("li");
-    winner.textContent = result;
-    banner.appendChild(winner)
-    playButton.disabled = true;
-    console.log(`Player Weapon: ${playerWeapon}, Machine Weapon: ${machineResult}, ${result}`);
 }
 
 playButton.addEventListener("click", play)
